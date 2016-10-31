78487ba15515d6a5d655d0f978dd679182f84500
gandjustas <gandjustas@mail.ru>
2016-10-31 04:43:52 +0300

    Added Workflow custom event demo.

---
+++ WorkflowCustomEventDemo/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="7fd01e4d-c74c-4e64-b868-695146e886b6" description="SharePoint App Feature" featureId="7fd01e4d-c74c-4e64-b868-695146e886b6" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Workflow Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="243f69a7-bd36-4bde-8c17-d7cb4bfb911d" />
+    <projectItemReference itemId="a2dc7ca6-4162-46bb-823a-1f38ca0417f3" />
+    <projectItemReference itemId="be45eab5-1cf5-46ee-ad6c-a4d94f73845a" />
+    <projectItemReference itemId="d085e80d-5c21-4326-b98e-c3b6227de4db" />
+    <projectItemReference itemId="6cb9e2ae-e218-447c-8908-53de8de79cb6" />
+    <projectItemReference itemId="a9040cc0-6ddb-447f-a016-e4af580384fa" />
+  </projectItems>
+</feature>
---
+++ WorkflowCustomEventDemo/Scripts/app.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path="typings/sharepoint/sharepoint.d.ts" />
+SP.SOD.executeFunc('sp.js', 'SP.ClientContext', () => {
+    var context = SP.ClientContext.get_current();
+
+    var web = context.get_web();
+
+    context.executeQueryAsync(() => {
+        SP.SOD.executeFunc('sp.workflowservices.js', 'SP.WorkflowServices', () => {
+
+            var servicesManager = SP.WorkflowServices.WorkflowServicesManager.newObject(context, web);
+
+
+            var deploymentService = servicesManager.getWorkflowDeploymentService();
+            var instanceService = servicesManager.getWorkflowInstanceService();
+            var subscriptionService = servicesManager.getWorkflowSubscriptionService();
+
+            var subs = subscriptionService.enumerateSubscriptions();
+            context.load(subs);
+            context.executeQueryAsync(() => {
+                var sub = subs.get_item(0);
+                var guid = instanceService.startWorkflow(sub, {});
+
+                context.executeQueryAsync(() => {
+                    var instance = instanceService.getInstance(<any>(guid.get_value()))
+
+                    var button = $get("sendevent");
+                    $addHandler(button, "click", e => {
+                        instanceService.publishCustomEvent(instance, "MyEvent", "");
+                        context.executeQueryAsync();
+                        e.preventDefault();
+                    });
+                });
+            });
+        });
+    });
+
+});
